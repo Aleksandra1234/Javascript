6f9ddbba541eb040edaf1022e0cd06f135dbf4e5
Aleksandra Karaseva <vavilkina.a.karaseva@gmail.com>
2021-07-24 10:12:38 +0300

    changes in 9th task

--- task_23/9.js
+++ task_23/9.js
@@ -1,23 +1,15 @@
+let amount = 0;
+
 let first = prompt('Подвижный холм песка в пустыне называется:\n А. Дюна\n Б. Утес\n В. Скала');
 if (first == 'A') {
-     first = 2;
-}
-else {
-    first = 0;
+    amount += 2;
     }
 let second = prompt('Чего не может торнадо?\nА. Стоять на месте\nБ. Поднять в воздух автомобиль\nВ. Вырвать с корнями дерево');
 if (second == 'A') {
-    second = 2;
+    amount += 2;
 }
-else {
-    second = 0;
-    }
 let third = prompt('Где находится яд у кобры?\nА. На кончике языка\nБ. В зубе\nВ. На хвосте');
 if (third == 'Б') {
-    third = 2;
+    amount += 2;
 }
-else {
-    third = 0;
-}
-let amount = first + second + third;
-alert(`Сумма набранных баллов составляет: ${amount}`)
+console.log(`Сумма набранных баллов составляет: ${amount}`)
